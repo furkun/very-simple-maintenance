2a5413d0095dc07db971111dc39b024fdadb0412
Furkan Uysal <81188796+furkun@users.noreply.github.com>
2022-03-14 13:03:27 +0300

    Create sm-template.php

---
+++ sm-template.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width">
+<title>Maintenance</title>
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<style>*{margin:0;padding:0}body{font-family:Georgia,Arial,Helvetica,Sans Serif;font-size:65.5%;background-color:#2c3e50}#content{font-size:150%;width:80%;margin:0 auto;padding:5% 0;text-align:center}#content p{font-size:1em;padding:.8em 0;color:#bdc3c7}h1,h2{color:#ecf0f1}h1{font-size:300%;padding:.5em 0}</style>
+</head>
+<body>
+<div id="content">
+<h1>Maintenance Mode</h1>
+<p>Our website is currently undergoing scheduled maintenance.</p>
+<p>Please try back again soon.</p>
+<p>Sorry for the inconvenience.</p>			
+</div>
+</body>
+</html>
